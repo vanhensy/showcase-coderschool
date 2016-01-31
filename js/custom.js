bf16f9d3c675ae7678e35f46ca93aad0090f2104
Vanhensy <anhvynv.vanhensy@gmail.com>
2016-01-31 20:22:47 +0700

    Auto set height for background

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,7 @@
 var screenHeight = window.innerHeight ; 
+document.getElementById("showcase").style.backgroundSize = 100 + "%" + " " + (screenHeight-70)/2 + "px";
 document.getElementById("center-block").style.minHeight = screenHeight + "px";
 document.getElementById("left-block").style.minHeight = screenHeight + "px";
 document.getElementById("right-block").style.minHeight = screenHeight + "px";
+document.getElementById("app-list").style.minHeight = (screenHeight - 170)/2 + "px"; 
 $('body').css({height: screenHeight + "px"});
