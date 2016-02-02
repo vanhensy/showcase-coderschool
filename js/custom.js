8b982e268cfae214858ba68d1f519448c91e96fd
Vanhensy <anhvynv.vanhensy@gmail.com>
2016-02-02 17:05:15 +0700

    eliminate some component to make it simpler

--- js/custom.js
+++ js/custom.js
@@ -7,11 +7,12 @@ document.getElementById("left-block").style.minHeight = screenHeight + "px";
 document.getElementById("right-block").style.minHeight = screenHeight + "px";
 document.getElementById("app-list").style.height = boxHeight; 
 document.getElementById("introduction").style.height = boxHeight; 
-var boxContentHeight = (screenHeight - 170)/2 - 40 + "px";
+var boxContentHeight = (screenHeight - 170)/2 - 20 + "px";
+var introTextHeight = (screenHeight - 170)/2 - 50 + "px";
 $('body').css({height: screenHeight + "px"});
 jQuery(document).ready(function($) {
 			  $('#introduction-text').slimScroll({
-				  height: '160px',
+				  height: introTextHeight,
                   color: '#fff'
 			  });
               $('#app-box').slimScroll({
